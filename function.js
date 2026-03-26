b5591065756dec4619c3aa9893e7dd50ac39ec8b
kavin1327 <gobalkavin007@gmail.com>
2026-03-26 19:24:25 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,34 @@
+const add = (a, b) => a + b;
+let result = add(10, 20);
+console.log(result);
+
+//30
+
+ const square = (num) => {
+  return num * num;
+};
+console.log(square(5)); 
+
+//25
+
+ const hello = () => {
+  console.log("Hello World!");
+};
+hello();
+
+//Hello World!
+
+const greet = (name) => {
+  console.log("HI " + name);
+};
+greet("Kavin");
+
+//Hii Kavin
+
+ const multiply = (a, b) => {
+  let result = a * b;
+  console.log("Multiplication " + result);
+};
+multiply(3, 4);
+
+// Multiplication 12
